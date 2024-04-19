fe0c7792168bcd253aba90b67bcff9f8c78f1af9
jatin-pandey01 <=>
2024-04-19 17:39:39 +0530

    Product screen done

---
+++ lib/screens/details/detail_image_slider.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class DetailImageSlider extends StatefulWidget {
+  final String image;
+  final Function (int) onChange;
+  const DetailImageSlider({super.key, required this.image, required this.onChange});
+
+  @override
+  State<DetailImageSlider> createState() => _DetailImageSliderState();
+}
+
+class _DetailImageSliderState extends State<DetailImageSlider> {
+    int currentSlide = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        SizedBox(
+          height: 250,
+          child: PageView(
+            allowImplicitScrolling: true,
+            onPageChanged: widget.onChange,
+            scrollDirection: Axis.horizontal,
+            physics: ClampingScrollPhysics(),
+            children: [
+              Image.asset(widget.image),
+              Image.asset(widget.image),
+              Image.asset(widget.image),
+            ],
+          ),
+        ),
+              
+      ],
+    );
+  }
+}
--- lib/screens/details/detail_screen.dart
+++ lib/screens/details/detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:ecommerce_app/models/constant.dart';
 import 'package:ecommerce_app/models/shopping_items.dart';
 import 'package:ecommerce_app/screens/details/detail_app_bar.dart';
+import 'package:ecommerce_app/screens/details/detail_image_slider.dart';
 import 'package:flutter/material.dart';
 
 class DetailScreen extends StatefulWidget {
@@ -12,14 +13,54 @@ class DetailScreen extends StatefulWidget {
 }
 
 class _DetailScreenState extends State<DetailScreen> {
+  int currentSlide = 0;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: contentColor,
       body: SafeArea(
         child: Column(
           children: [
+            //App Bar
             DetailAppBar(),
+            
+            //Image Slider
+            DetailImageSlider(image: widget.product.image,onChange: (value){
+              setState(() {
+                currentSlide = value;
+              });
+            },),
+            
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(
+                3, 
+                (index) => AnimatedContainer(
+                  duration: Duration(microseconds: 300),
+                  width: currentSlide == index ? 15 : 8,
+                  height: 8,
+                  margin: EdgeInsets.only(right: 3),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: currentSlide == index  ? Colors.black : Colors.transparent,
+                    border: Border.all(
+                      color:Colors.black 
+                    ) 
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 20,),
+            Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(20),
+                ),
+                color: Colors.white,
+              ),
+            )
           ],
         )
       ),
